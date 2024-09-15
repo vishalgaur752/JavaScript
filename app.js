e2656c68b93c39e75e11bf5bb142614ffd13af32
vishal gaur <vishalgaur752@gmail.com>
2024-09-15 14:42:13 +0530

    JavaScript

--- app.js
+++ app.js
@@ -7,7 +7,23 @@
 
 
 //template litrals.
-let pencilprice = 10;
-let erosorprice = 5;
-let output = `The total price is ${pencilprice + erosorprice} Rupees.`;
-console.log(output);
+// let pencilprice = 10;
+// let erosorprice = 5;
+// let output = `The total price is ${pencilprice + erosorprice} Rupees.`;
+// console.log(output);
+
+//Arithmetic Operators
+let a = 10;
+let b = 5;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+//unary operator
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
