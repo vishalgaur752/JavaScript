1598dc10d229d720c1ec3a98858607212fa0b29a
vishal gaur <vishalgaur752@gmail.com>
2024-09-15 14:31:57 +0530

    javaScript

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log("hello world");
-console.log("Vishal gaur");
+// console.log("hello world");
+// console.log("Vishal gaur");
+
+// let a = 5;
+// let b = 10;
+// console.log(a + b);
+
+
+//template litrals.
+let pencilprice = 10;
+let erosorprice = 5;
+let output = `The total price is ${pencilprice + erosorprice} Rupees.`;
+console.log(output);
