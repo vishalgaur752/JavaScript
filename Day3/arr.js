a2ce43e3b6c8ddfe5340bdd5cb40b95c8c0995bb
vishal gaur <vishalgaur752@gmail.com>
2024-09-17 14:51:52 +0530

    Arrays

--- Day3/arr.js
+++ Day3/arr.js
@@ -15,7 +15,19 @@
 //Array Methods
 //push(add to end), unshift(add to start), pop(delete from end & returns it) and shift(delete from start & returns it).
 // let cars = ["audi", "bmw", "XUV", "alto"];
-let followers = ["a","b","c"];
-let blocked = followers.shift();
-console.log(followers);
-console.log(blocked);
+// let followers = ["a","b","c"];
+// let blocked = followers.shift();
+// console.log(followers);
+// console.log(blocked);
+
+
+//Ques 1 For the giver start state of an array, change it to final form using methods.
+//start = ["jan", "july", "march", "aug"]
+//final = ["july", "june", "march", "aug"]
+
+let start = ["jan", "july", "march", "aug"];
+start.shift();
+start.shift();
+start.unshift("june");
+start.unshift("july");
+console.log(start);
