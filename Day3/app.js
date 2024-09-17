58f1cfc1fa4eec8b7a800b243c175b66d50445ac
vishal gaur <vishalgaur752@gmail.com>
2024-09-17 11:54:36 +0530

    javaScript

---
+++ Day3/app.js
@@ -0,0 +1,27 @@
+// let msg = "     hello     ";
+// let masage = msg.trim();
+// console.log(masage); 
+
+// let password = prompt("set your password");
+// let newPass = password.trim();
+// console.log(newPass);
+
+// let str = "iloveCoding";
+// let newStr = str.indexOf("love");
+// console.log(newStr)
+
+
+//Method Chaining
+// let msg = "   hello   "
+// let newMsg = msg.trim().toUpperCase();
+// console.log(newMsg);
+
+
+
+//Slice method
+let str = "IloveCoding";
+let slicedStr = str.slice(5);
+let partStr = str.slice(1, 5);
+console.log(slicedStr);
+console.log(partStr);
+
