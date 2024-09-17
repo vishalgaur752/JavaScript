8a292d02cd46c9d34f30af4b63f5dcae5d558f16
vishal gaur <vishalgaur752@gmail.com>
2024-09-17 14:29:54 +0530

    Arrays

--- Day3/app.js
+++ Day3/app.js
@@ -19,9 +19,34 @@
 
 
 //Slice method
-let str = "IloveCoding";
-let slicedStr = str.slice(5);
-let partStr = str.slice(1, 5);
-console.log(slicedStr);
-console.log(partStr);
+// let str = "IloveCoding";
+// let slicedStr = str.slice(5);
+// let partStr = str.slice(1, 5);
+// let subStr = str.slice(-6);
+// console.log(slicedStr);
+// console.log(partStr);
+// console.log(subStr);
 
+
+//Replace Method
+// let str = "IloveCodeing";
+// let replacedValue = str.replace("love", "do");
+// console.log(replacedValue);
+
+
+
+// //Question 1 for the given strig : let msg = "help!"; Trim it & convert it to uppercase.
+// let msg = "help!";
+// let TrimedNdUpperCase = msg.trim().toUpperCase();
+// console.log(TrimedNdUpperCase)
+
+
+// //Ques - 2 For the String -> let name = "ApnaCollege", predict the output for following:
+// let names = "ApnaCollege";
+// console.log(names.slice(4, 9));
+// console.log(names.indexOf("na"));
+// console.log(names.replace("Apna", "Our"));
+
+
+// //Ques - 3 Separate the "College" part in above string& replace 'i' with 't' in it.
+// console.log(names.slice(4).replace("l", "t"));
---
+++ Day3/arr.js
@@ -0,0 +1,21 @@
+// let students = ["aman", "vishal", "sudc"];
+// console.log(students);
+// console.log(students[0]);
+// console.log(students.length);  
+
+// let nums = [2, 4, 6, 8];
+
+
+//Arrays are mutable
+// let fruits = ["mango", "apple", "litchi"];
+// fruits[0] = "banana";
+// console.log(fruits);
+
+
+//Array Methods
+//push(add to end), unshift(add to start), pop(delete from end & returns it) and shift(delete from start & returns it).
+// let cars = ["audi", "bmw", "XUV", "alto"];
+let followers = ["a","b","c"];
+let blocked = followers.shift();
+console.log(followers);
+console.log(blocked);
