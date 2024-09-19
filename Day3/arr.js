eda38c795e0945d5b8d5fc814dc58ac98858c514
vishal gaur <vishalgaur752@gmail.com>
2024-09-19 17:49:55 +0530

    JavaScript

--- Day3/arr.js
+++ Day3/arr.js
@@ -42,8 +42,45 @@
 
 
 //slice in array
-let colors = ["red", "yellow", "blue", "orange", "pink", "white"];
-console.log(colors.slice());
-console.log(colors.slice(2));
-console.log(colors.slice(2, 3));
-console.log(colors.slice(-2));
+// let colors = ["red", "yellow", "blue", "orange", "pink", "white"];
+// console.log(colors.slice());
+// console.log(colors.slice(2));
+// console.log(colors.slice(2, 3));
+// console.log(colors.slice(-2));
+
+
+// spice in arrays
+// let color  = ["red", "yellow", "blue", "orange", "pink", "white"];
+// console.log(color.splice(2));
+// color.splice(4);
+
+
+//sort in arrays
+// color.sort();
+
+//ques 1 : For given start state of an array, change it to fnal form using splice.
+// let start = ["january", "july", "march", "august"];
+// start.splice(0, 2, "july", "june");
+// console.log(start);
+
+//Ques 2 : Return the index of the "javScript" from the given, if it was reversed.
+// let proLang = ["c", "c++", "html", "javaScript", "python", "java", "c#", "sql"];
+// console.log(proLang.reverse().indexOf("javaScript"));
+
+//Array References
+// let arr = ['a', 'b'];
+// let arrCopy = arr;
+// arrCopy;
+
+// Constant Array
+// const arr = [1, 2, 3];
+// arr = [1, 2, 3, 4];
+// console.log(arr)
+
+//Nested Arrays
+// let nums = [[1, 2], [5, 4, 5, 8], [7, 5]];
+// console.log(nums)
+
+//Ques : Create a nested array to show the following tic-tac-toe game state.
+let game = [['X' , '', 'O'], [ '', 'X', ''], ['O', '', 'X']];
+console.log(game);
