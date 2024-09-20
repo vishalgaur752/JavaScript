ae9737033095362d5872f8ca3736db4a98e69b19
vishal gaur <vishalgaur752@gmail.com>
2024-09-20 20:31:33 +0530

    Nested Loops - JavaScript

--- Day4/script.js
+++ Day4/script.js
@@ -4,20 +4,35 @@
 
 
 // print all odd nubers (1 to 15)
-for(let i = 1; i<=15; i++) {
-    if(i % 2 != 0) {
-        console.log(i);
-    }
-}
+// for(let i = 1; i<=15; i++) {
+//     if(i % 2 != 0) {
+//         console.log(i);
+//     }
+// }
 
 //print all even number (2 to 10)
-for(let i = 2; i<= 10; i++) {
-    if(i % 2 == 0) {
-        console.log(i);
-    }
-}
+// for(let i = 2; i<= 10; i++) {
+//     if(i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
 
 //print the multiplication table of 5
-for (let i = 1; i<=10; i++) {
-    console.log(`5 * ${i} = `, 5 * i);
+// for (let i = 1; i<=10; i++) {
+//     console.log(`5 * ${i} = `, 5 * i);
+// }
+
+// let n = prompt("write your number");
+// n = parseInt(n);
+// for (let i = n; i<=n*10; i =  i+n) {
+//     console.log(i);
+// } 
+
+
+//nested loops
+for (let i = 1; i<=3; i++) {
+    console.log(`outer loop ${i}`);
+    for (let j = 1; j<=3; j++) {
+        console.log(j);
+    }
 }
