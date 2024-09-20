70564f3420b092b3a06aa556b3ebaef068597185
vishal gaur <vishalgaur752@gmail.com>
2024-09-20 17:53:20 +0530

    Loops JavaScript

---
+++ Day4/script.js
@@ -0,0 +1,18 @@
+// for(let i = 1; i<=5; i++) {
+//     console.log(i);
+// }
+
+
+// print all odd nubers (1 to 15)
+for(let i = 1; i<=15; i++) {
+    if(i % 2 != 0) {
+        console.log(i);
+    }
+}
+
+//print all even number (2 to 10)
+for(let i = 2; i<= 10; i++) {
+    if(i % 2 == 0) {
+        console.log(i);
+    }
+}
